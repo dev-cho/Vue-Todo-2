f2c93282e8deb5815df17020bfe18578becfac54
Yongmin Cho <yong@Yongminui-MacBook-Pro.local>
2018-08-28 12:44:43 +0900

    Vue.js Example Todo

---
+++ src/components/Todo/TodoList.vue
@@ -0,0 +1,66 @@
+<template>
+	<section>
+		<transition-group name="list" tag="ul">
+			<li v-for="(item,index) in propsdata" :key="item" class="shadow">
+				<i class="checkBtn fa fa-check" aria-hidden="true"></i>
+				{{ item }}
+				<span class="removeBtn" type="button" @click="removeTodo(item,index)">
+					<i class="fa fa-trash-o" aria-hidden="true"></i>
+				</span>
+			</li>
+		</transition-group>
+	</section>
+</template>
+
+<script>
+export default {
+	props: ['propsdata'],
+	methods:{
+		removeTodo(item,index){
+			this.$emit('removeTodo',item,index);
+		}
+	}
+}
+</script>
+
+<style>
+ul{
+	list-style-type: none;
+	padding-left: 0px;
+	margin-top: 0;
+	text-align: left;
+}
+.list-item {
+	display: inline-block;
+	margin-right: 10px;
+}
+.list-move{
+	transition: transform 1s;
+}
+.list-enter-active, .list-leave-active {
+	transition: all 1s;
+}
+.list-enter, .list-leave-to {
+	opacity:0;
+	transform:translateY(30px);
+}
+li{
+	display: flex;
+	min-height:50px;
+	height: 50px;
+	line-height: 50px;
+	margin:0.5rem 0;
+	padding: 0 0.9rem;
+	background:white;
+	border-radius:5px;
+}
+.checkBtn{
+	line-height: 45px;
+	color:#62acde;
+	margin-right: 5px;
+}
+.removeBtn{
+	margin-left: auto;
+	color:#de4343;
+}
+</style>
